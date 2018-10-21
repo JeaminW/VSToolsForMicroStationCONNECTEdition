eb4549ea26687169dcd26c44ba5e4c126b802ba9
innovoCAD <martyrobbins@innovocad.com>
2018-10-21 09:58:24 -0700

    Importing Project

---
+++ git.bat
@@ -0,0 +1,8 @@
+echo "# VSTMC" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/innovocad/VSTMC.git
+git push -u origin master
+
+pause
